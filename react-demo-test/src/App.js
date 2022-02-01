051206054cc31f7ea5eb32cecd64fcb1e209f785
Philipp Lammerding <plammerding@jastech-solutions.de>
2022-02-01 00:59:14 +0000

    initial commit

---
+++ react-demo-test/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import AppBar from "components/AppBar";
+import MasterPage from "pages/MasterPage";
+
+const App = () => {
+  return (
+    <div className="App full-height">
+      <AppBar />
+      <MasterPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-demo-test/src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseURL = "Northwind_full_url/";
+
+const instance = axios.create({
+  baseURL
+});
+
+export const getTableData = async (params = { $top: 100, $skip: 0 }) => {
+  const { data } = await instance.get("/Categories", {
+    params
+  });
+
+  return data.d?.results || data.d || data.value;
+};
+
+export const getTableCount = async () => {
+  const { data } = await instance.get("/Categories/$count");
+  return data;
+};
